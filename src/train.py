4f64386de08a01df6918c999f40dfb516c8fc8f0
oscarl77 <oscarlaurens18@gmail.com>
2025-06-06 20:00:48 +0100

    Dcoumented code

--- src/train.py
+++ src/train.py
@@ -3,16 +3,15 @@ from src.utils.data_loader import get_training_set
 from src.models.Perceptron import Perceptron
 
 def train():
+    """Prepares and runs the training process for the perceptron model"""
     config = load_config()
     experiment_name = config["EXPERIMENT_NAME"]
-    EPOCHS = config["EPOCHS"]
     X, y = get_training_set()
 
-    perceptron = Perceptron(EPOCHS)
+    perceptron = Perceptron()
     theta, theta_0 = perceptron.train(X, y)
     experiment_details = {experiment_name: (theta.tolist(), theta_0.tolist())}
     update_config("SAVED_MODELS", experiment_details)
-
 
 if __name__ == "__main__":
     train()
