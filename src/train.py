cf4ff605c9f3a44df7906bc98c2e25c50713e8f5
oscarl77 <oscarlaurens18@gmail.com>
2025-06-06 01:37:06 +0100

    Implemented training algorithm for perceptron

--- src/train.py
+++ src/train.py
@@ -1,10 +1,13 @@
-import pandas as pd
-
+from src.config import config
 from src.utils.data_loader import get_training_set
+from src.models.Perceptron import Perceptron
 
 def train():
+    EPOCHS = config['EPOCHS']
     X, y = get_training_set()
 
+    perceptron = Perceptron(EPOCHS)
+    theta, theta_0 = perceptron.train(X, y)
 
 if __name__ == "__main__":
     train()
