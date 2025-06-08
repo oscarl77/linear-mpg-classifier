f487cf0ad89be702fafe183b9c6cc01b75132c42
oscarl77 <oscarlaurens18@gmail.com>
2025-06-09 00:41:37 +0100

    Fixed bug where previous experiments were overwritten

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -1,14 +1,21 @@
 import json
+import os
 
 def load_config(path="./config.json"):
     with open(path, "r") as f:
         return json.load(f)
 
-def update_config(key, value, path="./config.json"):
+def update_config(experiment_name, params, path="./config.json"):
     """Adds a new entry into the config file. In the context of this project
     it saves an experiment's weight vector and bias term.
     """
     config = load_config()
-    config[key] = value
+
+    if "SAVED_SVM_PARAMS" not in config:
+        config["SAVED_SVM_PARAMS"] = {}
+
+    config["SAVED_SVM_PARAMS"][experiment_name] = params
     with open(path, "w") as f:
         json.dump(config, f, indent=4)
+        #f.flush()
+        #os.fsync(f.fileno())
